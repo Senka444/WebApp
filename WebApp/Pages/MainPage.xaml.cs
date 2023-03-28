6aa7db0fcc4d34c5fc1ec0d49cad6dadbc2a6105
arsen <arsen@DESKTOP-MG932T0>
2023-03-28 07:50:03 +0300

    Добавьте файлы проекта.

---
+++ WebApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OneButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OnePage());
+        }
+
+        private void ManyUser_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ManyUser());
+        }
+    }
+}
